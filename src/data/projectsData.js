99587833075918c9d3615caf76cab9b491010e55
phallaboramey19 <phalla.boramey19@kit.edu.kh>
2023-08-21 01:36:41 +0700

    changing project detail data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,91 +1,92 @@
-import one from '../assets/svg/projects/one.svg'
-import two from '../assets/svg/projects/two.svg'
-import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
-import six from '../assets/svg/projects/six.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import eight from '../assets/svg/projects/eight.svg'
-import nine from '../assets/png/odoo3.png'
-import ten from '../assets/png/tetris.png'
-import eleven from '../assets/png/spotify.png'
-
+import one from "../assets/svg/projects/one.svg";
+import two from "../assets/svg/projects/two.svg";
+import three from "../assets/svg/projects/three.svg";
+import four from "../assets/svg/projects/four.svg";
+import five from "../assets/svg/projects/five.svg";
+import six from "../assets/svg/projects/six.svg";
+import seven from "../assets/svg/projects/seven.svg";
+import eight from "../assets/svg/projects/eight.svg";
+import nine from "../assets/png/odoo3.png";
+import ten from "../assets/png/flipflash.png";
+import eleven from "../assets/png/onlychat.png";
 
 export const projectsData = [
-    {
-        id: 1,
-        projectName: 'Odoo ERP System',
-        projectDesc: 'This project aims to migrate a legacy erp software to odoo erp system 13.0 and migrations of over a million record has to be done.',
-        tags: ['XML', 'Javascript', 'Python', 'Postgresql'],
-        code: 'https://gitlab.com/phalla.boramey19/odoo-13-upgrade',
-        demo: '',
-        image: nine
-    },
-    {
-        id: 2,
-        projectName: 'Tetris Video Game Clone',
-        projectDesc: 'The web Tetris video game clone is a project developed using React. This clone recreates the classic Tetris game, allowing users to experience the nostalgic gameplay in a modern web environment. ',
-        tags: ['HTML5', 'CSS3', 'Javascript', 'React JS'],
-        code: 'https://github.com/rachanapich/tetris',
-        demo: '',
-        image: ten
-    },
-    {
-        id: 3,
-        projectName: 'Spotify Clone',
-        projectDesc: 'This is a fullstack spotify clone that is built using React JS, Express JS and Spotify API. It allows users to login with their spotify account and play music.',
-        tags: ['HTML5', 'CSS3', 'React JS', 'Express JS'],
-        code: 'https://github.com/rachanapich/spotify-clone',
-        demo: '',
-        image: eleven
-    },
-    // {
-    //     id: 4,
-    //     projectName: 'Android Patient Tracker',
-    //     projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-    //     tags: ['Flutter', 'Firebase'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: four
-    // },
-    // {
-    //     id: 5,
-    //     projectName: 'E-Commerce App',
-    //     projectDesc: 'A Simple E-commerce application',
-    //     tags: ['React Native', 'Firebase'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: five
-    // },
-    // {
-    //     id: 6,
-    //     projectName: 'Uber Lite',
-    //     projectDesc: 'Uber clone',
-    //     tags: ['Flutter'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: six
-    // },
-    // {
-    //     id: 7,
-    //     projectName: 'Stock Market App',
-    //     projectDesc: 'A simple stock market API app',
-    //     tags: ['React', 'Redux', 'Bootstrap'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: seven
-    // },
-    // {
-    //     id: 8,
-    //     projectName: 'Car Pooling System',
-    //     projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-    //     tags: ['Flutter', 'React'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: eight
-    // },
-]
-
+  {
+    id: 1,
+    projectName: "Odoo ERP System",
+    projectDesc:
+      "This project aims to migrate a legacy erp software to odoo erp system 13.0 and migrations of over a million record has to be done.",
+    tags: ["XML", "Javascript", "Python", "Postgresql"],
+    code: "https://gitlab.com/phalla.boramey19/odoo-13-upgrade",
+    demo: "",
+    image: nine,
+  },
+  {
+    id: 2,
+    projectName: "FlipFlash",
+    projectDesc:
+      "This is a bootcamp project that done as group about creating desktop application using electron js. This is an self study application that allow user to make study to do list and also create quick task to memorize the lesson.",
+    tags: ["Vue JS", "Electron JS", "Node JS", "MongoDB"],
+    code: "https://github.com/rachanapich/FlipFlash/tree/flipflash-backend",
+    demo: "",
+    image: ten,
+  },
+  {
+    id: 3,
+    projectName: "OnlyChat",
+    projectDesc:
+      "Fronted : react ts material ui Figma Vercel Backend : Java spring boot , rest api , postgresql, railway for hosting",
+    tags: ["React JS", "Springboot", "REST Api", "Postgresql"],
+    code: "https://github.com/rachanapich/OnlyChat",
+    demo: "",
+    image: eleven,
+  },
+  // {
+  //     id: 4,
+  //     projectName: 'Android Patient Tracker',
+  //     projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
+  //     tags: ['Flutter', 'Firebase'],
+  //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     image: four
+  // },
+  // {
+  //     id: 5,
+  //     projectName: 'E-Commerce App',
+  //     projectDesc: 'A Simple E-commerce application',
+  //     tags: ['React Native', 'Firebase'],
+  //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     image: five
+  // },
+  // {
+  //     id: 6,
+  //     projectName: 'Uber Lite',
+  //     projectDesc: 'Uber clone',
+  //     tags: ['Flutter'],
+  //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     image: six
+  // },
+  // {
+  //     id: 7,
+  //     projectName: 'Stock Market App',
+  //     projectDesc: 'A simple stock market API app',
+  //     tags: ['React', 'Redux', 'Bootstrap'],
+  //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     image: seven
+  // },
+  // {
+  //     id: 8,
+  //     projectName: 'Car Pooling System',
+  //     projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+  //     tags: ['Flutter', 'React'],
+  //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     image: eight
+  // },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below
